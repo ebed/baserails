7c97f2ce64b1e06768410968edef806b54d52fe2
Rodrigo Merino <rodrigo@desafiolatam.com>
2021-07-04 17:01:40 -0400

    Adding setting for semantic logger

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,6 +10,11 @@ module Speter
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.colorize_logging = true
+    config.rails_semantic_logger.quiet_assets = true
+    config.rails_semantic_logger.processing = true
+    config.rails_semantic_logger.rendered = true
+    config.rails_semantic_logger.ap_options = { multiline: true }
 
     # Configuration for the application, engines, and railties goes here.
     #
